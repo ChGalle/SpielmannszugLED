1ae97356e80faa198e2c72d367036d795cce3883
ChGalle <info@christoph-galle.de>
2019-04-09 10:19:39 +0200

    Interrupt definition und Interruptroutine hinzugefügt.

--- main/main.ino
+++ main/main.ino
@@ -14,61 +14,70 @@ CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
 
 void setup() {
+        attachInterrupt(0, debounceInterrupt, FALLING); //Modetaster an Interrupt0
 
-  delay(3000); // Warten, damit der Strip nicht flackert.
+        delay(3000); // Warten, damit der Strip nicht flackert.
 
-  // 420 LEDS an Pin 3, beginnend bei index 0
-  FastLED.addLeds<NEOPIXEL, 3>(leds, 0, NUM_LEDS_PER_STRIP);
+        // 420 LEDS an Pin 3, beginnend bei index 0
+        FastLED.addLeds<NEOPIXEL, 3>(leds, 0, NUM_LEDS_PER_STRIP);
 
-  // 420 LEDS an Pin 5, beginnend bei index 420
-  FastLED.addLeds<NEOPIXEL, 5>(leds, NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+        // 420 LEDS an Pin 5, beginnend bei index 420
+        FastLED.addLeds<NEOPIXEL, 5>(leds, NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
 
-  //Setze die Gesamthelligkeit
-  FastLED.setBrightness( BRIGHTNESS );
+        //Setze die Gesamthelligkeit
+        FastLED.setBrightness( BRIGHTNESS );
 
+}
+
+//Debouce Routine für den Modetaster
+void debounceInterrupt() {
+        detachInterrupt(0);
+        delayMicroseconds(15000);
+        attachInterrupt(0, debounceInterrupt, FALLING); //Modetaster an Interrupt0
+        // do something
 }
 
 void animation_rot_weiss() {
 
-  //Einblenden von Rot und Weiß, Pixel für Pixel
-  for (int i = 0; i < 207; i++) {
-    // Setze die LED auf Rot
-    leds[i] = CRGB::Red;
-    FastLED.show();
-  }
-  for (int i = 207; i < 414; i++) {
-    // Setze die LED auf Weiß
-    leds[i] = CRGB( 108, 108, 60);
-    FastLED.show();
-  }
-  for (int i = 414; i < 621; i++) {
-    // Setze die LED auf Weiß
-    leds[i] = CRGB( 108, 108, 60);
-    FastLED.show();
-  }
-  for (int i = 621; i < 840; i++) {
-    // Setze die LED auf Rot
-    leds[i] = CRGB::Red;
-    FastLED.show();
-  }
+        //Einblenden von Rot und Weiß, Pixel für Pixel
+        for (int i = 0; i < 207; i++) {
+                // Setze die LED auf Rot
+                leds[i] = CRGB::Red;
+                FastLED.show();
+        }
+        for (int i = 207; i < 414; i++) {
+                // Setze die LED auf Weiß
+                leds[i] = CRGB( 108, 108, 60);
+                FastLED.show();
+        }
+        for (int i = 414; i < 621; i++) {
+                // Setze die LED auf Weiß
+                leds[i] = CRGB( 108, 108, 60);
+                FastLED.show();
+        }
+        for (int i = 621; i < 840; i++) {
+                // Setze die LED auf Rot
+                leds[i] = CRGB::Red;
+                FastLED.show();
+        }
 }
 
 void warm_weiss() {
 
-  //Einblenden von Warm Weiß, Pixel für Pixel, in rückwärtiger Richtung
-  for (int i = 840; i >= 0; i--) {
-    // Setze die LED auf Warm Weiß
-    leds[i] = CRGB( 70, 50, 15);
-    FastLED.show();
-  }
+        //Einblenden von Warm Weiß, Pixel für Pixel, in rückwärtiger Richtung
+        for (int i = 840; i >= 0; i--) {
+                // Setze die LED auf Warm Weiß
+                leds[i] = CRGB( 70, 50, 15);
+                FastLED.show();
+        }
 }
 
 void loop() {
-  //Zeige Animation Rot-Weiß für 10 Sekunden
-  animation_rot_weiss();
-  delay(10000);
-  //Zeige Warm Weiß für 10 Minuten
-  warm_weiss();
-  delay(600000);
+        //Zeige Animation Rot-Weiß für 10 Sekunden
+        animation_rot_weiss();
+        delay(10000);
+        //Zeige Warm Weiß für 10 Minuten
+        warm_weiss();
+        delay(600000);
 
 }
